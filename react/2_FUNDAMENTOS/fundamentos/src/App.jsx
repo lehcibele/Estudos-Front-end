d4085827772f856a2c0263cd4a92657a41613124
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-01-29 15:40:02 -0300

    feat: instalando o react no projeto fundamentos

---
+++ react/2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -0,0 +1,17 @@
+// Componentes
+import FirstComponent from './components/FirstComponent';
+
+// CSS
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Meu primeiro projeto em React usando Vit</h1>
+      <FirstComponent/>
+    </>
+  )
+}
+
+export default App;
